e46d0419d0a546e7c9b05cba882f0a5f15c96c2e
taeho kim <reinf92@naver.com>
2021-07-28 13:57:36 +0900

    아이템 재등록

---
+++ item.py
@@ -0,0 +1,30 @@
+MAX_DURABILITY = 100
+MIN_DURABILITY = 1
+COST = 33
+
+class Item():
+
+    def __init__(self):
+        self.level = 1
+        self.durability = MAX_DURABILITY
+        self.status = True
+
+    def reset(self):
+        self.level = 1
+        self.durability = MAX_DURABILITY
+        self.status = True
+
+    def failed(self):
+        self.durability -= COST
+
+        if (self.durability == 1):
+            self.status = False
+
+    def success(self):
+        self.durability = MAX_DURABILITY
+        self.level = self.level + 1
+
+    def toString(self):
+        return 'Level :' + self.level + ', Durability : ' + self.durability + ', Status : ' + self.status
+        
+            
